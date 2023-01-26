3acc44453662cc2c32fb3653b0015120f618e592
Sahil Naliyadhara <sahilnaliyadhara110@gmail.com>
2023-01-26 16:21:43 +0530

    Completed All Functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import AddTransaction from "./Components/AddTransaction";
+import Balance from "./Components/Balance";
+import Header from "./Components/Header";
+import IncomeExpenses from "./Components/IncomeExpenses";
+import TransactionList from "./Components/TransactionList";
+import { GlobalProvider } from "./context/GlobalState";
+
 function App() {
-  return <>HELLO WORLD</>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </div>
+  );
 }
 
 export default App;
